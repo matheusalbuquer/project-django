58ce8be6f2f1fd4d59d9d463a312ae283a565fb0
matheus albuquer <mj898926@gmail.com>
2025-02-21 22:48:14 -0300

    primeiro commit

---
+++ learning_logs/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Topic(models.Model):
+    """Um assunto sobre qual o usuario está aprendendo"""
+
+    text =  models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Devolce uma representaçao em Sring do modelo"""
+        return self.text
+    
+
+class Entry(models.Model):
+    """Algo específico aprendido sobre um assunto"""
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Devolve uma representação String do modelo"""
+        return self.text[:50] + '...'  # Corrigindo a indentação aqui!
