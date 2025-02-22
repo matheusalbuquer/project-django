c1bba2030c80e22dacc53a89756a2d62082d01a8
matheus albuquer <mj898926@gmail.com>
2025-02-22 14:32:51 -0300

    add topics

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Topic
 # Create your views here.
+
+def index(request):
+    """Página princiapl do learning log"""
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Mostra todos os assunto"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request,'learning_logs/topics.html', context)
