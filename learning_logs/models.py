8c55c30ce683c79390e04255cb6308f431809981
matheus albuquer <mj898926@gmail.com>
2025-03-03 06:41:42 -0300

    Linking an owner to each topic

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Topic(models.Model):
     """Um assunto sobre qual o usuario está aprendendo"""
 
     text =  models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owern = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """Devolce uma representaçao em Sring do modelo"""
